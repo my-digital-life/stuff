e203bad02e465310eafc5eea885d90562497a72d
my-digital-life <152349349+my-digital-life@users.noreply.github.com>
2024-03-18 02:06:19 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,14 +2,10 @@
 
 # sudo curl -sS https://raw.githubusercontent.com/my-digital-life/stuff/main/setup.sh | bash
 # set up the timezone
-# sudo timedatectl set-timezone "America/Toronto"
+sudo timedatectl set-timezone "America/Toronto"
 
-# Update
-# sudo apt update
-# Remove snapd and ubuntu-advantage-tools
-# sudo apt -y purge ubuntu-advantage-tools snapd && sudo apt -y autoremove
-# upgrade
-#sudo apt -y dist-upgrade
+# Upgrade
+sudo apt update && sudo apt -y dist-upgrade
 
 # install SSH
 sudo apt -y install ssh openssh-server
