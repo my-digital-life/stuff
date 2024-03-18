63380fb95bd7e2531fa146c23e8e4e48a776ee43
my-digital-life <152349349+my-digital-life@users.noreply.github.com>
2024-03-18 00:47:41 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,21 +2,21 @@
 
 # sudo curl -sS https://raw.githubusercontent.com/my-digital-life/stuff/main/setup.sh | bash
 # set up the timezone
-sudo timedatectl set-timezone "America/Toronto"
+timedatectl set-timezone "America/Toronto"
 
 # Update
-sudo apt update
+apt update
 # Remove snapd and ubuntu-advantage-tools
-sudo apt -y purge ubuntu-advantage-tools snapd && sudo apt -y autoremove
+apt -y purge ubuntu-advantage-tools snapd && sudo apt -y autoremove
 # upgrade
-sudo apt -y dist-upgrade
+apt -y dist-upgrade
 
 # install SSH
-sudo apt -y install ssh openssh-server
+apt -y install ssh openssh-server
 systemctl enable ssh
 systemctl start ssh
 
 # config ssh
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 
